bdcb87a6a6e008a7a2e0bd052b6ca0a1fc6b9705
Nikhil <nikhilbonthu2@gmail.com>
2026-09-22 15:09:29 -0500

    Load current matching module in browser

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-import { calculateMatchScore, filterDogs } from "./match.js";
+import { calculateMatchScore, filterDogs } from "./match.js?v=af481f6";
 
 const supabase = createClient(
   "https://emtnkyfpzryozlzwtqve.supabase.co",
